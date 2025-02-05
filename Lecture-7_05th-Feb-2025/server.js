386422f21ffc2b6e21a8bed22570d2827892981a
Jaiveer2004 <singhjaiveer1005@gmail.com>
2025-02-05 15:51:02 +0530

    Added 5th Feb 2025

--- Lecture-7_05th-Feb-2025/server.js
+++ Lecture-7_05th-Feb-2025/server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8085;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+// Standard way to serve static files
+app.use('/static', express.static('public'));
+app.use('/assets', express.static('assets'));
+
+app.use('/images', express.static('Images'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.get('/user', (req, res) => {
-    res.render('users.html');
+app.get('/users', (req, res) => {
+    res.send('About Us');
 });
 
 app.listen(port, () => {
